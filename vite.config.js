c8e979d0084b87632f200b0e1216be8fec5ce72e
sowjanyadevakumar <sowjanyadevakumar@gmail.com>
2025-09-13 03:04:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your GitHub repository name
+  base: '/sowjanyadevakumar.com/', // Replace with your GitHub repository name
   server: {
     proxy: {
       '/api': {
